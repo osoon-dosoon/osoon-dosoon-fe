feb621bbeb415c666885c9412e34084d690d2ad4
hwanny-son <hwanny.son@gmail.com>
2026-01-11 19:36:49 +0900

    App.tsx 파일 에러 수정

--- osoondosoon/src/app/App.tsx
+++ osoondosoon/src/app/App.tsx
@@ -1,4 +1,4 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import BoardsListPage from "@/pages/BoardsListPage";
 import BoardDetailPage from "@/pages/BoardDetailPage";
 import BoardFormPage from "@/pages/BoardFormPage";
